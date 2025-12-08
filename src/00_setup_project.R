e97bf504af9b9cc7e5929845af87420f08eef9d8
MarcKevinSchneider <schneider.mk2803@gmail.com>
2025-12-08 14:46:37 +0100

    initial commit

---
+++ src/00_setup_project.R
@@ -0,0 +1,16 @@
+#' @name 00_setup_project.R
+#' @date 08.12.2025
+#' @author Marc Kevin Schneider
+#' @contact Schnei7q@students.uni-marburg.de
+#' 
+#' @description
+#' Setup script for the analysis "Establishing the Minimum Number of Validation 
+#' Points Needed for Reliable Species Distribution Model Assessment 
+#' Under Varying Conditions". 
+#' 
+#' 
+
+
+# ================================================================
+# 1. Load required packages
+# ================================================================
