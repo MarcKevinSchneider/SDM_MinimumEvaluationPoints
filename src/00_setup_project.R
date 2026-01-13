634abe259658ec8354f29fa93b62217fe367e303
MarcKevinSchneider <schneider.mk2803@gmail.com>
2026-01-13 14:40:55 +0100

    introduced a balanced and a normal 10,000 random background points dataset

--- src/00_setup_project.R
+++ src/00_setup_project.R
@@ -32,7 +32,7 @@ library(blockCV)          # for some of the sampling strategies
 library(Metrics)          # for the evaluation metrics 
 library(tidyverse)        # general functions
 library(geodata)          # for the download of the border data
-library(mecofun)          # for the binary cutoff of the predictions
+library(PresenceAbsence)  # for the binary cutoff of the predictions
 
 # ================================================================
 # 2. Folder structure
--- src/02_sampling_presence_points.R
+++ src/02_sampling_presence_points.R
@@ -24,22 +24,12 @@ samp_strats <- c("random", "block")
 for (strat in samp_strats){
   source(paste0(envrmt$path_src, "/functions/", strat, "_sampling_function.R"))
 }
-#source(paste0(envrmt$path_src, "/functions/sampling_function.R"))
+# sourcing the function for creating the background points
+source(paste0(envrmt$path_src, "/functions/background_points_sampling.R"))
 
 # ================================================================
-# 2. Sampling the presence, absence and background points using the sampling strategies
+# 2. Setting the parameters
 # ================================================================
-
-#params <- expand.grid(
-#  sp   = as.character(c("VS01", "VS02", "VS03", "VS04", "VS05", "VS06",
-#                        "VS07", "VS08", "VS09", "VS10")),
-#  fit  = as.character(c("0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7",
-#                        "0.8", "0.9", "1")),
-#  n    = as.numeric(seq(1, 300, 1)),
-#  iter = as.numeric(seq(1,10,1)),
-#  stringsAsFactors = FALSE
-#)
-
 
 # only testing on a small subset of the data right now
 params <- expand.grid(
@@ -50,9 +40,41 @@ params <- expand.grid(
   stringsAsFactors = FALSE
 )
 
+# ================================================================
+# 3. Sampling the background points randomly
+# ================================================================
+
+# parameters for the 10,000 randomy background points
+# dont need iteration and sample size here
+bck_params <- expand.grid(
+  sp   = as.character(c("VS01", "VS02", "VS03", "VS04", "VS05")),
+  fit  = as.character(c("0.1", "0.2", "0.3", "0.4", "0.5")),
+  stringsAsFactors = FALSE
+)
+
+# sample 10,000 background points for each species and then one iteration per fit
+lapply(1:nrow(bck_params), function(i){
+  bck_sampling(bck_params$sp[i], bck_params$fit[i])
+})
+
+# ================================================================
+# 4. Sampling a balanced amount of background points
+# ================================================================
+
+# sample a specified number of background points for each species, each fit and 
+# then a few iterations of it
+# allows for a balanced presence-absence and background point evaluation
+lapply(1:nrow(params), function(i){
+  balanced_bck_sampling(params$sp[i], params$fit[i], params$n[i], params$iter[i])
+})
+
+# ================================================================
+# 5. Sampling the presence-absence points using the sampling strategies
+# ================================================================
+
 # loop over all parameters
 lapply(1:nrow(params), function(i){
   #random_sampling(params$sp[i], params$fit[i], params$n[i], params$iter[i])
   #cluster_sampling(params$sp[i], params$fit[i], params$n[i], params$iter[i], plot=TRUE)
-  block_sampling(params$sp[i], params$fit[i], params$n[i], params$iter[i])
+  #block_sampling(params$sp[i], params$fit[i], params$n[i], params$iter[i])
 })
