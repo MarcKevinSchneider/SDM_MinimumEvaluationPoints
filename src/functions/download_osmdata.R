e39ab77883760f28875ab96025f942f099566bd3
MarcKevinSchneider <schneider.mk2803@gmail.com>
2026-01-23 12:47:03 +0100

    include the first version of the effort driven sampling function and update the downloadosm script to include the creation of a Vic/NWS/ACT National Park dataset

--- src/functions/download_osmdata.R
+++ src/functions/download_osmdata.R
@@ -5,6 +5,7 @@
 #' 
 #' @description
 #' Downloads the OpenStreetMap data for the study area of the analysis.
+#' Also used for transforming the geospatial data from other sources.
 #' 
 
 
@@ -88,31 +89,8 @@ terra::writeRaster(road_mask, filename = paste0(envrmt$path_osm, "/road_sampling
                    overwrite = TRUE)
 
 
-
 # ================================================================
-# 5. Download the OSM population data
-# ================================================================
-
-# build query
-place_query <- opq(bbox = bbox,timeout = 180) |>
-  add_osm_feature(key = "place",value = c("city", "town", "village"))
-
-# download the data
-places_osm <- osmdata_sf(place_query)
-
-# extract population extents, transform, etc.
-places_sf <- places_osm$osm_points
-places_sf <- sf::st_transform(places_sf, terra::crs(landscape))
-places_sf <- sf::st_crop(places_sf, sf::st_bbox(ext_poly))
-
-# new folder for the osm data
-osm_dir <- paste0(envrmt$path_data, "/osm")
-if (!dir.exists(osm_dir)) dir.create(osm_dir, recursive = TRUE)
-sf::write_sf(places_sf,paste0(osm_dir, "/places_osm.gpkg"))
-
-
-# ================================================================
-# 6. Download the OSM county border data
+# 5. Download the OSM county border data
 # ================================================================
 
 # build query
@@ -134,52 +112,72 @@ sf::write_sf(counties_sf,
 
 
 # ================================================================
-# 7. Download the OSM national park data
+# 6. Transform the National Park data
 # ================================================================
 
-# build query
-#park_query <- opq(bbox = bbox,timeout = 180) |>
-#  add_osm_feature(key = "boundary", value = "protected_area") |>
-#  add_osm_feature(key = "protect_class",value = "2")
+# Victoria national park data
+# from https://discover.data.vic.gov.au/dataset/parks-and-conservation-reserves-parkres
+vic <- sf::st_read(paste0(envrmt$path_osm, "/PARKRES.shp"), quiet=TRUE)
 
-park_query <- opq(bbox = bbox,timeout = 180) |>
-  add_osm_feature(key = "boundary",value = "protected_area")
+# Australian capital territory national park data
+# from https://actmapi-actgov.opendata.arcgis.com/datasets/82a13a6bba6c4bce91e78a495fb754aa_0/explore?location=0.017175%2C0.000000%2C1.83
+act <- sf::st_read(paste0(envrmt$path_osm, "/ACTGOV_TP_OVERLAY_ZONE_POLY.shp"), quiet=TRUE)
 
-# download the data
-parks_osm <- osmdata_sf(park_query)
 
-# extract park extents, transform, etc.
-#parks_sf <- parks_osm$osm_multipolygons
-parks_sf <- dplyr::bind_rows(parks_osm$osm_multipolygons,parks_osm$osm_polygons)
+# NSW national park data
+# from https://datasets.seed.nsw.gov.au/dataset/9bad468a-c2a6-4c90-bfaa-8ae67cbb50ef2/resource/33fb1ee3-9090-4cae-a4e7-c4e3c83ba5c8/download/tenure_npws_estateinternalboundaries.zip
+nsw <- sf::st_read(paste0(envrmt$path_osm, "/NPWS_EstateInternalBoundaries.shp"),
+                   quiet=TRUE)
 
-parks_sf <- sf::st_transform(parks_sf, terra::crs(landscape))
-parks_sf <- sf::st_crop(parks_sf, sf::st_bbox(ext_poly))
+# check the names of the national parks
+unique(vic$AREA_TYPE)
+unique(act$OVERLAY_PR)
+unique(nsw$TYPE)
 
-parks_np <- parks_sf[grepl("national park", parks_sf$name, ignore.case = TRUE),]
+# initial filtering to just the national parks
+vic_np <- vic |>
+  filter(AREA_TYPE == "NATIONAL PARK - SCHEDULE 2, NATIONAL PARKS ACT")
 
-parks_np <- parks_np[parks_np$boundary == "protected_area", ]
+act_np <- act |>
+  filter(OVERLAY_PR == "National Park")
 
-names(parks_np) <- make.unique(names(parks_np))
-parks_np <- sf::st_make_valid(parks_np)
-parks_np <- sf::st_collection_extract(parks_np, "POLYGON")
+nsw_np <- nsw |>
+  filter(TYPE == "NATIONAL PARK")
 
-parks_np <- parks_np |>
-  dplyr::select(
-    name,
-    operator,
-    boundary,
-    protect_class,
-    geometry
-  )
 
-out_file <- paste0(osm_dir, "/test_australia_national_parks_osm.gpkg")
+# filter and clean up 
+vic_np_clean <- vic_np |>
+  dplyr::select(name = NAME, type = AREA_TYPE, geometry = geometry) |>
+  dplyr::mutate(state = "Victoria")
 
-if (file.exists(out_file)) file.remove(out_file)
+# same for NSW
+nsw_np_clean <- nsw_np |>
+  dplyr::select(name = NAME, type = TYPE, geometry = geometry) |>
+  dplyr::mutate(state = "NSW")
 
-sf::st_write(
-  parks_np,
-  dsn   = out_file,
-  layer = "national_parks",
-  quiet = FALSE
-)
+# same for NSW
+act_np_clean <- act_np |>
+  dplyr::select(name = DISTRICT_N, type = OVERLAY_PR, geometry = geometry) |>
+  dplyr::mutate(state = "ACT")
+
+# get the crs from the vic dataset
+target_crs <- sf::st_crs(vic_np_clean)
+
+
+# transfrom to vic crs
+if(sf::st_crs(nsw_np_clean) != target_crs) {
+  nsw_np_clean <- sf::st_transform(nsw_np_clean, target_crs)
+}
+
+# transfrom to vic crs
+if(sf::st_crs(act_np_clean) != target_crs) {
+  act_np_clean <- sf::st_transform(act_np_clean, target_crs)
+}
+
+# bind the three datasets
+combined_np <- dplyr::bind_rows(vic_np_clean, nsw_np_clean, act_np_clean)
+
+# save as geopackage
+sf::st_write(combined_np, paste0(envrmt$path_osm, "/nationalparks_vic_nsw_act.gpkg"))
+
 
--- src/functions/effortDriven_sampling_function.R
+++ src/functions/effortDriven_sampling_function.R
@@ -1,5 +1,5 @@
 #' @name effortDriven_sampling_function.R
-#' @date XX.XX.XXXX
+#' @date 23.01.2026
 #' @author Marc Kevin Schneider
 #' @contact Schnei7q@students.uni-marburg.de
 #' 
@@ -21,3 +21,130 @@ set.seed(2962)
 # ================================================================
 # 2. Effort Driven Sampling function
 # ================================================================
+
+effort_sampling <- function(species_name, fit, sample_p, iter){
+  '
+  Purpose: Samples presence-absence points using an effort-driven sampling approach,
+           e.g. sampling is strongly biased towards populated areas
+  
+  
+  Parameters:
+  ----------------------------
+  
+  species_name: str
+    Name of the species; example: "VS01", "VS02"
+    
+  fit: str
+    Name of the goodness of fit; example: "0.1", "0.2"
+    
+  sample_p: int
+    Number of sample points
+    
+  iter: str
+    What iteration the code is on
+    
+  
+  Returns:
+  ---------------------------
+  A presence-absence dataset with an effort-driven sampling approach
+  
+  '
+  
+  # 1. Ensuring data structure
+  #--------------------------------------------------------
+  species_name <- as.character(species_name)
+  fit <- as.character(fit)
+  sample_p <- as.numeric(sample_p)
+  iter <- as.character(iter)
+  
+  
+  # 2. Correctly reading and preparing the data
+  #--------------------------------------------------------
+  
+  # reading the species and landscape data
+  species <- readRDS(paste0(envrmt$path_VirtualSpecies, "/", species_name, ".rds"))
+  landscape <- terra::rast(paste0(envrmt$path_ADM, "/", species_name, "/", species_name,
+                                  "_", "Fit_", fit, ".tif"))
+  
+  # trim to remove NAs to the east of the study area
+  landscape <- terra::trim(landscape)
+  
+  # extracting the occurrence data
+  presence <- terra::unwrap(species[[4]])
+  
+  # multiply by two to get equal amounts of presence and absence points
+  sample_p_2 = sample_p * 2
+  
+  
+  # 3. Preparing the population density data
+  #--------------------------------------------------------
+  
+  # read the population density data
+  # from https://www.abs.gov.au/statistics/people/population/regional-population/2023-24#data-downloads
+  pop_density <- terra::rast(paste0(envrmt$path_osm, "/pop_density_vic_nsw_act.tif"))
+  
+  # reproject
+  pop_density <- terra::project(pop_density, presence)
+  #print("Reprojected...")
+  
+  # resample to match species grid
+  pop_density <- terra::resample(pop_density, presence, method = "bilinear")
+  #print("Resampled...")
+  
+  # mask again just to be sure
+  pop_density <- terra::mask(pop_density, presence)
+  #print("Masked...")
+  
+  
+  # replace NA and negative values
+  pop_density[is.na(pop_density)] <- 0
+  pop_density[pop_density < 0] <- 0
+  
+  # log-scale to reduce cities dominating the data
+  #pop_density <- log1p(pop_density)
+  
+  # normalise to sum = 1 
+  pop_density <- pop_density / terra::global(pop_density, "sum", na.rm = TRUE)[1,1]
+  #print("Normalised...")
+  
+  
+  # 4. Sampling near the population centers
+  #--------------------------------------------------------
+  
+  # strongly biased sampling towards population centers
+  pres_abs_points <- sampleOccurrences(presence, n = sample_p_2, type = "presence-absence",
+                                       replacement = FALSE, sample.prevalence = 0.5, 
+                                       bias = "manual", bias.strength = 100,
+                                       weights=pop_density)
+  
+  
+  # 5. Final formatting of the data
+  #--------------------------------------------------------
+  
+  # convert to dataframe
+  pres_abs_df <- as.data.frame(pres_abs_points$sample.points)
+  
+  # convert to stars vector object
+  pres_abs_sf <- sf::st_as_sf(pres_abs_df, coords = c("x", "y"), 
+                              crs = terra::crs(landscape),remove = F)
+  
+  # extracting the data for the presence-points
+  species_data_extr <- terra::extract(landscape, pres_abs_sf)
+  species_data_compl <- cbind(pres_abs_sf, species_data_extr)
+  #print(species_data_compl)
+  
+  sample_p <- as.character(sample_p)
+  
+  #print("Extracted the layer data...")
+  
+  # 6. Saving the data
+  #--------------------------------------------------------
+  
+  # creating directory for the presence absence data
+  dir_pres <- paste0(envrmt$path_pre_abs_points, "/EffortDriven/", species_name, "/", sample_p)
+  if(!dir.exists(dir_pres)) dir.create(dir_pres, recursive = TRUE)
+  # saving the presence absence data
+  sf::write_sf(species_data_compl, paste0(dir_pres, "/", species_name, "_Fit_", 
+                                          fit, "_Iteration_", iter, "_Pres_Abs.gpkg"))
+
+}
